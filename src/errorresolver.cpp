c23c7f945fb857081e8d44dedbf5e9f0d4f12d1e
Jeremy Dumais <jeremydumais@hotmail.com>
2022-02-01 21:33:14 -0500

    Work on the ErrorResolver class

--- src/errorresolver.cpp
+++ src/errorresolver.cpp
@@ -1,4 +1,7 @@
 #include "errorresolver.h"
+#include "smtpclienterrors.h"
+#include "socketerrors.h"
+#include "sslerrors.h"
 
 using namespace jed_utils;
 using namespace std::literals::string_literals;
@@ -8,23 +11,98 @@ ErrorResolver::ErrorResolver(int pErrorCode)
       mErrorMessage(""s)
 {
     switch (mErrorCode) {
-        case -1:
+        case SOCKET_INIT_SESSION_CREATION_ERROR:
             mErrorMessage = "Unable to create the socket"s;
         break;
-        case -2:
+        case SOCKET_INIT_SESSION_CONNECT_ERROR:
             mErrorMessage = "Unable to connect to the socket"s;
         break;
-        case -3:
+        case SOCKET_INIT_SESSION_CONNECT_TIMEOUT:
             mErrorMessage = "The connection attempt has timed out"s;
         break;
-        case -4:
+        case SOCKET_INIT_SESSION_WINSOCKET_STARTUP_ERROR:
             mErrorMessage = "Unable to start WSA (Winsock)"s;
         break;
-        case -5:
+        case SOCKET_INIT_SESSION_WINSOCKET_GETADDRINFO_ERROR:
             mErrorMessage = "Unable to get address info (Winsock)"s;
         break;
-        case -6:
+        case SOCKET_INIT_SESSION_GETHOSTBYNAME_ERROR:
             mErrorMessage = "Unable to get host by name"s;
+        break;
+        case SOCKET_INIT_CLIENT_SEND_EHLO_ERROR:
+            mErrorMessage = "Unable to send the EHLO command"s;
+        break;
+        case SOCKET_INIT_CLIENT_SEND_EHLO_TIMEOUT:
+            mErrorMessage = "The EHLO command has timed out"s;
+        break;
+        case SOCKET_INIT_CLIENT_SEND_STARTTLS_ERROR:
+            mErrorMessage = "Unable to send the STARTTLS command"s;
+        break;
+        case SOCKET_INIT_CLIENT_SEND_STARTTLS_TIMEOUT:
+            mErrorMessage = "The STARTTLS command has timed out"s;
+        break;
+        case SSL_CLIENT_STARTTLS_INITSSLCTX_ERROR:
+            mErrorMessage = "Unable to initialize the SSL Context"s;
+        break;
+        case SSL_CLIENT_STARTTLS_BIONEWSSLCONNECT_ERROR:
+            mErrorMessage = "Unable to create the SSL BIO"s;
+        break;
+        case SSL_CLIENT_STARTTLS_WIN_CERTOPENSYSTEMSTORE_ERROR:
+            mErrorMessage = "Unable to open the Windows Certificate System Store"s;
+        break;
+        case SSL_CLIENT_STARTTLS_CTX_SET_DEFAULT_VERIFY_PATHS_ERROR:
+            mErrorMessage = "Unable to open the specifies that the default locations from which CA certificates are loaded"s;
+        break;
+        case SSL_CLIENT_STARTTLS_BIO_CONNECT_ERROR:
+            mErrorMessage = "Unable to connect SSL BIO"s;
+        break;
+        case SSL_CLIENT_STARTTLS_BIO_HANDSHAKE_ERROR:
+            mErrorMessage = "Unable to handshake with SSL BIO"s;
+        break;
+        case SSL_CLIENT_STARTTLS_GET_CERTIFICATE_ERROR:
+            mErrorMessage = "Unable to get peer certificate"s;
+        break;
+        case SSL_CLIENT_STARTTLS_VERIFY_RESULT_ERROR:
+            mErrorMessage = "Unable to verify the result of chain verification"s;
+        break;
+        case SSL_CLIENT_INITSECURECLIENT_ERROR:
+            mErrorMessage = "Unable to EHLO the server via the secure channel"s;
+        break;
+        case SSL_CLIENT_INITSECURECLIENT_TIMEOUT:
+            mErrorMessage = "The EHLO command via the secure channel timed out"s;
+        break;
+        case CLIENT_AUTHENTICATE_ERROR:
+            mErrorMessage = "Unable to authenticate with the credentials provided"s;
+        break;
+        case CLIENT_AUTHENTICATE_TIMEOUT:
+            mErrorMessage = "The authentication to the server has timed out"s;
+        break;
+        case CLIENT_AUTHENTICATE_NONEED:
+            mErrorMessage = "The authentication is not needed on the server"s;
+        break;
+        case CLIENT_AUTHENTICATION_METHOD_NOTSUPPORTED:
+            mErrorMessage = "The authentication method selected is not supported by the server"s;
+        break;
+        case CLIENT_SENDMAIL_MAILFROM_ERROR:
+            mErrorMessage = "The MAIL FROM command return an error"s;
+        break;
+        case CLIENT_SENDMAIL_MAILFROM_TIMEOUT:
+            mErrorMessage = "The MAIL FROM command timed out"s;
+        break;
+        case CLIENT_SENDMAIL_RCPTTO_ERROR:
+            mErrorMessage = "The RCPT TO command return an error"s;
+        break;
+        case CLIENT_SENDMAIL_RCPTTO_TIMEOUT:
+            mErrorMessage = "The RCPT TO command timed out"s;
+        break;
+        case CLIENT_SENDMAIL_DATA_ERROR:
+            mErrorMessage = "The DATA command return an error"s;
+        break;
+        case CLIENT_SENDMAIL_DATA_TIMEOUT:
+            mErrorMessage = "The DATA command timed out"s;
+        break;
+        case CLIENT_SENDMAIL_HEADERFROM_ERROR:
+            mErrorMessage = "The From header command return an error"s;
         break;
         default:
             mErrorMessage = "No message correspond to this error code"s;
--- test/smtpclient_unittest/errorresolver_unittest.cpp
+++ test/smtpclient_unittest/errorresolver_unittest.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include "../../src/errorresolver.h"
+#include "../../src/smtpclienterrors.h"
+#include "../../src/socketerrors.h"
+#include "../../src/sslerrors.h"
 
 using namespace jed_utils;
 using namespace std::literals::string_literals;
@@ -26,39 +29,188 @@ TEST(ErrorResolver_getErrorMessage, WithNonExistantCode_ReturnNoMessage)
     ASSERT_EQ("No message correspond to this error code"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusOne_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_CREATION_ERROR_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-1);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_CREATION_ERROR);
     ASSERT_EQ("Unable to create the socket"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusTwo_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_CONNECT_ERROR_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-2);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_CONNECT_ERROR);
     ASSERT_EQ("Unable to connect to the socket"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusThree_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_CONNECT_TIMEOUT_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-3);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_CONNECT_TIMEOUT);
     ASSERT_EQ("The connection attempt has timed out"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusFour_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_WINSOCKET_STARTUP_ERROR_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-4);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_WINSOCKET_STARTUP_ERROR);
     ASSERT_EQ("Unable to start WSA (Winsock)"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusFive_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_WINSOCKET_GETADDRINFO_ERROR_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-5);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_WINSOCKET_GETADDRINFO_ERROR);
     ASSERT_EQ("Unable to get address info (Winsock)"s, errorResolver.getErrorMessage());
 }
 
-TEST(ErrorResolver_getErrorMessage, WithCodeMinusSix_ReturnValidMessage)
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_SESSION_GETHOSTBYNAME_ERROR_ReturnValidMessage)
 {
-    ErrorResolver errorResolver(-6);
+    ErrorResolver errorResolver(SOCKET_INIT_SESSION_GETHOSTBYNAME_ERROR);
     ASSERT_EQ("Unable to get host by name"s, errorResolver.getErrorMessage());
 }
 
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_CLIENT_SEND_EHLO_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SOCKET_INIT_CLIENT_SEND_EHLO_ERROR);
+    ASSERT_EQ("Unable to send the EHLO command"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_CLIENT_SEND_EHLO_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SOCKET_INIT_CLIENT_SEND_EHLO_TIMEOUT);
+    ASSERT_EQ("The EHLO command has timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_CLIENT_SEND_STARTTLS_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SOCKET_INIT_CLIENT_SEND_STARTTLS_ERROR);
+    ASSERT_EQ("Unable to send the STARTTLS command"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSOCKET_INIT_CLIENT_SEND_STARTTLS_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SOCKET_INIT_CLIENT_SEND_STARTTLS_TIMEOUT);
+    ASSERT_EQ("The STARTTLS command has timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_INITSSLCTX_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_INITSSLCTX_ERROR);
+    ASSERT_EQ("Unable to initialize the SSL Context"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_BIONEWSSLCONNECT_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_BIONEWSSLCONNECT_ERROR);
+    ASSERT_EQ("Unable to create the SSL BIO"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_WIN_CERTOPENSYSTEMSTORE_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_WIN_CERTOPENSYSTEMSTORE_ERROR);
+    ASSERT_EQ("Unable to open the Windows Certificate System Store"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_CTX_SET_DEFAULT_VERIFY_PATHS_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_CTX_SET_DEFAULT_VERIFY_PATHS_ERROR);
+    ASSERT_EQ("Unable to open the specifies that the default locations from which CA certificates are loaded"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_BIO_CONNECT_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_BIO_CONNECT_ERROR);
+    ASSERT_EQ("Unable to connect SSL BIO"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_BIO_HANDSHAKE_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_BIO_HANDSHAKE_ERROR);
+    ASSERT_EQ("Unable to handshake with SSL BIO"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_GET_CERTIFICATE_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_GET_CERTIFICATE_ERROR);
+    ASSERT_EQ("Unable to get peer certificate"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_STARTTLS_VERIFY_RESULT_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_STARTTLS_VERIFY_RESULT_ERROR);
+    ASSERT_EQ("Unable to verify the result of chain verification"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_INITSECURECLIENT_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_INITSECURECLIENT_ERROR);
+    ASSERT_EQ("Unable to EHLO the server via the secure channel"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithSSL_CLIENT_INITSECURECLIENT_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(SSL_CLIENT_INITSECURECLIENT_TIMEOUT);
+    ASSERT_EQ("The EHLO command via the secure channel timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_AUTHENTICATE_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_AUTHENTICATE_ERROR);
+    ASSERT_EQ("Unable to authenticate with the credentials provided"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_AUTHENTICATE_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_AUTHENTICATE_TIMEOUT);
+    ASSERT_EQ("The authentication to the server has timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_AUTHENTICATE_NONEED_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_AUTHENTICATE_NONEED);
+    ASSERT_EQ("The authentication is not needed on the server"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_AUTHENTICATION_METHOD_NOTSUPPORTED_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_AUTHENTICATION_METHOD_NOTSUPPORTED);
+    ASSERT_EQ("The authentication method selected is not supported by the server"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_MAILFROM_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_MAILFROM_ERROR);
+    ASSERT_EQ("The MAIL FROM command return an error"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_MAILFROM_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_MAILFROM_TIMEOUT);
+    ASSERT_EQ("The MAIL FROM command timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_RCPTTO_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_RCPTTO_ERROR);
+    ASSERT_EQ("The RCPT TO command return an error"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_RCPTTO_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_RCPTTO_TIMEOUT);
+    ASSERT_EQ("The RCPT TO command timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_DATA_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_DATA_ERROR);
+    ASSERT_EQ("The DATA command return an error"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_DATA_TIMEOUT_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_DATA_TIMEOUT);
+    ASSERT_EQ("The DATA command timed out"s, errorResolver.getErrorMessage());
+}
+
+TEST(ErrorResolver_getErrorMessage, WithCLIENT_SENDMAIL_HEADERFROM_ERROR_ReturnValidMessage)
+{
+    ErrorResolver errorResolver(CLIENT_SENDMAIL_HEADERFROM_ERROR);
+    ASSERT_EQ("The From header command return an error"s, errorResolver.getErrorMessage());
+}
